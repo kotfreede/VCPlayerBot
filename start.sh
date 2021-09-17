f19d6c1994bbab869493d263f39b4df70814855f
KOT BOTS <88032123+kotfreede@users.noreply.github.com>
2021-09-17 19:43:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Thor-Ragnarokk/VCPlayerBot /VCPlayerBot
+git clone https://github.com/kotfreede/VCPlayerBot /VCPlayerBot
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
